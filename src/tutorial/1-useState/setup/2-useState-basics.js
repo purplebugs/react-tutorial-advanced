c2bb1622f1d73c1a07a36696af207c2903925887
Anita Lipsky <anita@purplebugs.com>
2021-12-31 09:47:15 +0100

    handleClick to update value

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -8,9 +8,16 @@ const UseStateBasics = () => {
 
   const [text, setText] = useState("random title");
 
+  const handleClick = () => {
+    setText("hello world");
+  };
+
   return (
     <React.Fragment>
       <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
     </React.Fragment>
   );
 };
