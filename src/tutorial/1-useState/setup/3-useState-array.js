64ed40dd8fb06504c0cd4ea731a19443ee8edf18
Anita Lipsky <anita@purplebugs.com>
2021-12-31 11:05:11 +0100

    useState array, alt syntax, default example

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
+  const [people, setPeople] = React.useState([]); // set to empty array as default
   return <h2>useState array example</h2>;
 };
 
