2e799bd5ab6701d2a0e060b1e871cb2d0a6bfd69
Anita Lipsky <anita@purplebugs.com>
2021-12-31 11:08:55 +0100

    destructure array and return in component

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,8 +2,20 @@ import React from "react";
 import { data } from "../../../data";
 
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState([]); // set to empty array as default
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data); // set to empty array as default
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            {name}
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
