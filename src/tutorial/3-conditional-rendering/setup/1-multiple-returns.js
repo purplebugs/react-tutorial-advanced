4ac22ebe089795c66fc9784cdd22ae5253269767
Anita Lipsky <anita@purplebugs.com>
2022-01-01 08:36:09 +0100

    console.log conditional loading of user

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => {
+        const { login } = user;
+        console.log(user);
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
   }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
   return <h2>multiple returns</h2>;
 };
 
